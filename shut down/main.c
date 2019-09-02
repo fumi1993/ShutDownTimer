d1c3680753b4b87c2239e5aca95d7d08bfa768fb
fumi1993 <47338032+fumi1993@users.noreply.github.com>
2019-09-02 20:59:26 -0300

    Add files via upload

---
+++ shut down/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+//programa para apagar la pc
+int main()
+{
+  int choice;
+
+    printf("seleccione cuando apagar la pc \n");
+    printf("se apaga en 30 mins :1 \n");
+    printf("se apaga en 1 hora :2 \n");
+    printf("se apaga en 1 hora 30 mins :3 \n");
+    printf("se apaga en 2 horas :4 \n");
+    printf("se apaga en 3 horas :5 \n");
+    printf("CANCELAR APAGADO :6 \n");
+    printf("en caso de NO poner ninguna de las anteriores no se realiza accion \n");
+    scanf("%d",&choice);
+
+  switch(choice)
+{
+    case 1: printf("se apaga en 30 mins ");
+    system("C:\\WINDOWS\\System32\\shutdown /s -t 1800");
+        break;
+    case 2: printf("se apaga en 1 hora ");
+    system("C:\\WINDOWS\\System32\\shutdown /s -t 3600");
+        break;
+    case 3: printf("se apaga en 1 hora 30 mins ");
+    system("C:\\WINDOWS\\System32\\shutdown /s -t 5400");
+        break;
+    case 4: printf("se apaga en 2 horas ");
+    system("C:\\WINDOWS\\System32\\shutdown /s -t 7200");
+        break;
+    case 5: printf("se apaga en 3 horas ");
+    system("C:\\WINDOWS\\System32\\shutdown /s -t 10800");
+        break;
+    case 6: printf("Se Cancela El Apagado ");
+    system("C:\\WINDOWS\\System32\\shutdown /a");
+        break;
+    default: printf("no se realiza accion ");
+        break;
+}
+
+
+   return 0;
+}
