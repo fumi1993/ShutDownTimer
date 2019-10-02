c9d9eeba9c411ce364fe5b7d474d4a0eaf53d01d
fumi1993 <47338032+fumi1993@users.noreply.github.com>
2019-10-02 16:51:40 -0300

    Add files via upload

--- shut down/main.c
+++ shut down/main.c
@@ -11,7 +11,8 @@ int main()
     printf("se apaga en 1 hora 30 mins :3 \n");
     printf("se apaga en 2 horas :4 \n");
     printf("se apaga en 3 horas :5 \n");
-    printf("CANCELAR APAGADO :6 \n");
+    printf("se apaga en 5 horas :6 \n");
+    printf("CANCELAR APAGADO :7 \n");
     printf("en caso de NO poner ninguna de las anteriores no se realiza accion \n");
     scanf("%d",&choice);
 
@@ -32,10 +33,13 @@ int main()
     case 5: printf("se apaga en 3 horas ");
     system("C:\\WINDOWS\\System32\\shutdown /s -t 10800");
         break;
-    case 6: printf("Se Cancela El Apagado ");
+     case 6: printf("se apaga en 5 horas ");
+    system("C:\\WINDOWS\\System32\\shutdown /s -t 18000");
+        break;
+    case 7: printf("Se Cancela El Apagado ");
     system("C:\\WINDOWS\\System32\\shutdown /a");
         break;
-    default: printf("no se realiza accion ");
+    default: printf("no se realiza accion \a ");
         break;
 }
 
